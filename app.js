bdd726423072b2bf5473167623d7690d72f6e1b0
Abdinajib Liban <abdinajibliban@gmail.com>
2021-01-04 23:58:08 -0500

    added require modules for engineer intern and manager to app.js

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ var jest = require("jest");
 var inquirer = require("inquirer");
 var fs = require("file-system");
 var Employee = require("./lib/employee");
+var Engineer = require("./lib/engineer");
+var Intern= require("./lib/intern");
+var Manager = require("./lib/manager")
+
 
 
