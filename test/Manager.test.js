a96c045c14eb2ebd8da1ac12bfb631fc37a02c7c
Abdinajib Liban <abdinajibliban@gmail.com>
2021-01-05 23:10:06 -0500

    completed test folder

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,20 +1,20 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+var manager = require("../lib/manager");
+//var Employee = require("../lib/Employee");
 
-test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+test("Can set office number via varructor argument", () => {
+  var testValue = 100;
+  var e = new manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
-  const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  var testValue = "Manager";
+  var e = new manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  var testValue = 100;
+  var e = new manager("Foo", 1, "test@test.com", 100);
   expect(e.getOfficeNumber()).toBe(testValue);
 });
